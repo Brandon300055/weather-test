1501d9580976781e73883a04c7df2333c4fcbbb9
Brandon Stewart <brandonstewart300055@gmail.com>
2021-04-25 13:05:35 -0600

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    {{--    <title>{{ config('app.name', 'Weather APP) }}</title>--}}
+    <meta name="description" content="weather">
+    <meta name="keywords" content="weather">
+    <meta name="viewport" content="width=device-width, initial-scale=0.9">
+
+    <meta name="author" content="Brandon Stewart">
+
+    <!-- Favicon-->
+    {{--    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">--}}
+
+    <!-- App Styles CSS-->
+    <link rel="stylesheet" rel="preload" as="style" href="{{ asset('css/app.css') }}" >
+
+
+    @yield('page-css')
+
+</head>
+<body class="antialiased">
+
+@yield('content')
+
+<footer class="footer">
+    <div class="">
+        <div class="copy-right">Copyright © {{ date("Y") }}</div>
+    </div>
+</footer>
+
+@yield('page-scripts')
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('page-scripts')
+    @parent
+@endsection
+
+@section('page-css')
+    @parent
+@endsection
+
+@section('content')
+
+        <div>
+            <h1>She Works!</h1>
+        </div>
+
+@endsection
+
+
